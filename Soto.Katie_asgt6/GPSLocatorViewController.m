e97afa64c48eb037911d221497e0e6168f4b9caa
KSoto <KReneSoto@gmail.com>
2012-11-16 15:33:42 -0800

    Adding More Visuals

---
+++ Soto.Katie_asgt6/GPSLocatorViewController.m
@@ -0,0 +1,43 @@
+//
+//  GPSLocatorViewController.m
+//  Soto.Katie_asgt6
+//
+//  Created by Lion User on 16/11/2012.
+//  Copyright (c) 2012 Katie Soto. All rights reserved.
+//
+
+#import "GPSLocatorViewController.h"
+
+@interface GPSLocatorViewController ()
+
+@end
+
+@implementation GPSLocatorViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
---
+++ Soto.Katie_asgt6/ManualLocatorViewController.m
@@ -0,0 +1,43 @@
+//
+//  ManualLocatorViewController.m
+//  Soto.Katie_asgt6
+//
+//  Created by Lion User on 16/11/2012.
+//  Copyright (c) 2012 Katie Soto. All rights reserved.
+//
+
+#import "ManualLocatorViewController.h"
+
+@interface ManualLocatorViewController ()
+
+@end
+
+@implementation ManualLocatorViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
